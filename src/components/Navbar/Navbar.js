f6576e001ca5b97f5d1f76023d2dc0ff44fa5cd8
MdSalman2022 <salman15-4523@diu.edu.bd>
2023-03-04 21:14:14 +0600

    updated navbar from salman

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
                         <li onClick={()=>setNav('talk')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'talk' && 'text-accent'}`}>Talk to Lawyer</span></a></li> 
                         <li onClick={()=>setNav('ask')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'ask' && 'text-accent'}`}>Ask Lawyer</span></a></li>
                         <li onClick={()=>setNav('property')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'property' && 'text-accent'}`}>Property</span></a></li>
-                        <li><Link to="/" className={`cursor-pointer btn bg-transparent border-white text-white hover:bg-white hover:text-base-100 `}>Login</Link></li>
+                        <Link to="/login" className={`cursor-pointer btn bg-transparent border-white text-white hover:bg-white hover:text-base-100 `}>Login</Link>
                     </ul>
                     </div>
                     <Link to="/"><img className='w-32' src="https://i.ibb.co/nPDh7PX/ninja-lawyer-red.png" alt="" /></Link>
@@ -29,7 +29,7 @@ function Navbar() {
                         <li onClick={()=>setNav('talk')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'talk' && 'text-accent'}`}>Talk to Lawyer</span></a></li> 
                         <li onClick={()=>setNav('ask')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'ask' && 'text-accent'}`}>Ask Lawyer</span></a></li>
                         <li onClick={()=>setNav('property')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'property' && 'text-accent'}`}>Property</span></a></li>
-                        <li onClick={()=>setNav('property')} ><span className={`cursor-pointer btn bg-transparent border-white text-white hover:bg-white hover:text-base-100 `}>Login</span></li>
+                        <Link to="/login" onClick={()=>setNav('property')} ><span className={`cursor-pointer btn bg-transparent border-white text-white hover:bg-white hover:text-base-100 `}>Login</span></Link>
                     </ul>
                 </div>
             </div>
