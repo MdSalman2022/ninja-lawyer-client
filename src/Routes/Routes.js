7a263b1c541675b58ae6cb7fdf125aedafc0d8bc
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-04 20:29:01 +0600

    added login to routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom'; 
-import Main from '../layout/Main';
-import Home from '../pages/Home/Home';
-
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import LoginPage from "../pages/UserAuthentication/Login/Login";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>, 
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-        ]
-    }
-
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <LoginPage></LoginPage>,
+      },
+    ],
+  },
+]);
