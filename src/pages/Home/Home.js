377db8766736f9af7797ce5b7cb51b0b3fc995dd
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-04 20:19:30 +0600

    added login comp

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import Middle from "../../components/MiddleSection/Middle";
+import Login from "../../components/UserAuthentication/Login/Login";
 
 function Home() {
   return (
     <div>
       <HeroSection />
       <Middle />
+      <Login />
     </div>
   );
 }
