78af072089e0f33375a0b586f0e8838e002deb2f
MdSalman2022 <salman15-4523@diu.edu.bd>
2023-03-04 20:03:51 +0600

    navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 
 function Navbar() {
@@ -17,10 +18,10 @@ function Navbar() {
                         <li onClick={()=>setNav('talk')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'talk' && 'text-accent'}`}>Talk to Lawyer</span></a></li> 
                         <li onClick={()=>setNav('ask')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'ask' && 'text-accent'}`}>Ask Lawyer</span></a></li>
                         <li onClick={()=>setNav('property')} ><a><span className={`cursor-pointer transition-all duration-300 hover:text-accent font-semibold ${nav === 'property' && 'text-accent'}`}>Property</span></a></li>
-                        <li onClick={()=>setNav('property')} ><span className={`cursor-pointer btn bg-transparent border-white text-white hover:bg-white hover:text-base-100 `}>Login</span></li>
+                        <li><Link to="/" className={`cursor-pointer btn bg-transparent border-white text-white hover:bg-white hover:text-base-100 `}>Login</Link></li>
                     </ul>
                     </div>
-                    <a className=""><img className='w-32 ' src="https://i.ibb.co/nPDh7PX/ninja-lawyer-red.png" alt="" /></a>
+                    <Link to="/"><img className='w-32' src="https://i.ibb.co/nPDh7PX/ninja-lawyer-red.png" alt="" /></Link>
                 </div>
                 
                 <div className="navbar-end hidden lg:flex">
