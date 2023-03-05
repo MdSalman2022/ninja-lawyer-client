867274f8547d5cccab9dc6f88c90e3bb035b42e9
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-05 20:25:33 +0600

    changes to firebase.config

---
+++ src/assets/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxwj1wMRzjPJqdQ5612mHf3PCLM7RD_0E",
+  authDomain: "ninjalawyersweb.firebaseapp.com",
+  projectId: "ninjalawyersweb",
+  storageBucket: "ninjalawyersweb.appspot.com",
+  messagingSenderId: "407640334252",
+  appId: "1:407640334252:web:151ef68cdc8784514f2409",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
