8ebc272467983f3f9e6ef17e71115f3c357e8fc7
MdSalman2022 <salman15-4523@diu.edu.bd>
2023-03-05 15:36:52 +0600

    responsive on navbar and herosection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,6 +3,7 @@ import { Player } from '@lottiefiles/react-lottie-player';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick"; 
+import './HeroSection.css'
 
 function HeroSection() {
 
@@ -28,17 +29,17 @@ function HeroSection() {
                 <div className="text-3xl lg:text-5xl font-bold ">Welcome To <span className='text-accent'>Ninja Lawyer</span></div>
                     <div className="">Welcome to our technology aiming to eliminate your legal worries. Consult anything and everything legal with <br /> experienced Legal professionals available 24X7 to talk to you.</div>
                 </article>
-                <Slider className="slick-slider slider-dots" {...settings}>
+                <Slider {...settings}>
                     <div>
-                        <div className="flex flex-col md:grid grid-cols-5 justify-items-center content-center place-content-center place-items-center">
-                            <div className='col-span-1'>
-                                <Player className='w-80 lg:w-[300px]' autoplay loop src="https://assets7.lottiefiles.com/packages/lf20_rdkrsaca.json"></Player>
+                        <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-items-center content-center place-content-center place-items-center">
+                            <div className='col-span-2 md:col-span-1 lg:col-span-1'>
+                                <Player className='w-80 md:w-52 lg:w-[300px]' autoplay loop src="https://assets7.lottiefiles.com/packages/lf20_rdkrsaca.json"></Player>
                             </div> 
-                            <div className='col-span-3'>
-                                <Player className='w-80 lg:w-[700px]  ' autoplay loop src="https://assets9.lottiefiles.com/packages/lf20_qwATcU.json"> </Player>
+                            <div className='col-span-1 md:col-span-1 lg:col-span-3'>
+                                <Player className='w-40 md:w-80 lg:w-[700px]  ' autoplay loop src="https://assets9.lottiefiles.com/packages/lf20_qwATcU.json"> </Player>
                             </div>
-                            <div className='col-span-1'>
-                                <Player className='w-80 lg:w-[300px]' autoplay loop src="https://assets7.lottiefiles.com/packages/lf20_3adn32pc.json"> </Player>
+                            <div className='col-span-1 md:col-span-1 lg:col-span-1'>
+                                <Player className='w-40 md:w-52 lg:w-[300px]' autoplay loop src="https://assets7.lottiefiles.com/packages/lf20_3adn32pc.json"> </Player>
                             </div>
                         </div>
                     </div>
