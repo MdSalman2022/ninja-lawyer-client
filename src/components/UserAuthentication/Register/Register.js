426336ceb331bc33c97f88a8d3d210c6982611b5
MdSalman2022 <salman15-4523@diu.edu.bd>
2023-03-05 19:37:15 +0600

    added register page from main

--- src/components/UserAuthentication/Register/Register.js
+++ src/components/UserAuthentication/Register/Register.js
@@ -110,12 +110,12 @@ export default function Register() {
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account?{" "}
-                  <a
-                    href="#"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  <Link
+                    to="/login"
+                    className="font-medium  text-accent hover:underline dark:text-primary-500"
                   >
                     Login here
-                  </a>
+                  </Link>
                 </p>
               </form>
             </div>
