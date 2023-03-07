45c514eb60690ed78878a2d1f1e8c8c6e9e344ca
MdSalman2022 <salman15-4523@diu.edu.bd>
2023-03-07 23:19:55 +0600

    navigation button updated with find lawyer

---
+++ src/pages/DemoPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DemoPage() {
+    return (
+        <div className='bg-primary dark:bg-base-100 h-screen flex items-center justify-center'>
+            <p className='text-base-100 dark:text-primary text-2xl'>Demo Page for Testing</p>
+        </div>
+    )
+}
+
+export default DemoPage
