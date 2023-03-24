01e97e940081017d40868fd9a7205472f3e6a248
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-03-25 02:37:09 +0600

    adding demo admin

--- src/pages/Test/TestApiPage.js
+++ src/pages/Test/TestApiPage.js
@@ -9,41 +9,41 @@ import { sendToServer } from "../../components/UserAuthentication/Login/LoginPos
 import { auth } from "../../assets/firebase.config";
 
 function TestApiPage() {
-  useEffect(() => {
-    for (let i = 0; i < 100; i++) {
-      console.log(users_data[i]);
-      async function emailLogin() {
-        const email = users_data[i].email;
-        const password = users_data[i].password;
-        const name = `${users_data[i].First_Name} ${users_data[i].Last_Name}`;
-        const phone = `${users_data[i].phone}`;
+  // useEffect(() => {
+  //   for (let i = 0; i < 100; i++) {
+  //     console.log(users_data[i]);
+  //     async function emailLogin() {
+  //       const email = users_data[i].email;
+  //       const password = users_data[i].password;
+  //       const name = `${users_data[i].First_Name} ${users_data[i].Last_Name}`;
+  //       const phone = `${users_data[i].phone}`;
 
-        createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            console.log(user);
-            // Post to sever
-            const postData = {
-              UID: user.uid,
-              email: email,
-              name: name,
-              phone: phone,
-            };
-            const msg = sendToServer(user.id, postData);
-            return msg;
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage, "Code: ", errorCode);
-            const msg = errorCode;
-            return msg;
-          });
-      }
-      // emailLogin();
-    }
-  }, []);
+  //       createUserWithEmailAndPassword(auth, email, password)
+  //         .then((userCredential) => {
+  //           // Signed in
+  //           const user = userCredential.user;
+  //           console.log(user);
+  //           // Post to sever
+  //           const postData = {
+  //             UID: user.uid,
+  //             email: email,
+  //             name: name,
+  //             phone: phone,
+  //           };
+  //           const msg = sendToServer(user.id, postData);
+  //           return msg;
+  //         })
+  //         .catch((error) => {
+  //           const errorCode = error.code;
+  //           const errorMessage = error.message;
+  //           console.log(errorMessage, "Code: ", errorCode);
+  //           const msg = errorCode;
+  //           return msg;
+  //         });
+  //     }
+  //     // emailLogin();
+  //   }
+  // }, []);
 
   return (
     <div className="bg-primary dark:bg-base-100">
