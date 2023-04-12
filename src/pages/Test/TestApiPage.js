a97d1e25745242cee1e0bd805ddb7aa3df3728be
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-04-12 19:30:52 +0600

    update

--- src/pages/Test/TestApiPage.js
+++ src/pages/Test/TestApiPage.js
@@ -8,38 +8,36 @@ import React, { useContext, useEffect, useState } from "react";
 import { app } from "../../assets/firebase.config";
 import { getDatabase, ref, set, update, onDisconnect } from "firebase/database";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import { auth } from "../../assets/firebase.config";
+import { updateProfile } from "firebase/auth";
 
 export default function TestApiPage() {
   const { user } = useContext(AuthContext);
-  const [isOnline, setIsOnline] = useState(false);
 
   // for online/offline
   useEffect(() => {
-    const db = getDatabase(app);
-
-    function writeUserData(uid) {
-      if (user && user.uid) {
-        console.log(user.uid, "-0-");
-        console.log("-----", uid);
-        set(ref(db, "lawyers/" + uid), {
-          isOnline: true,
-          uid: uid,
-        });
-
-        const userRef = ref(db, "lawyers/" + uid);
-        onDisconnect(userRef)
-          .update({
-            isOnline: false,
-          })
-          .then(() => {
-            console.log("OnDisconnect event set up successfully");
-          })
-          .catch((error) => {
-            console.error("Error setting up onDisconnect event:", error);
-          });
-      }
-    }
-
+    // const db = getDatabase(app);
+    // function writeUserData(uid) {
+    //   if (user && user.uid) {
+    //     console.log(user.uid, "-0-");
+    //     console.log("-----", uid);
+    //     set(ref(db, "lawyers/" + uid), {
+    //       isOnline: true,
+    //       uid: uid,
+    //     });
+    //     const userRef = ref(db, "lawyers/" + uid);
+    //     onDisconnect(userRef)
+    //       .update({
+    //         isOnline: false,
+    //       })
+    //       .then(() => {
+    //         console.log("OnDisconnect event set up successfully");
+    //       })
+    //       .catch((error) => {
+    //         console.error("Error setting up onDisconnect event:", error);
+    //       });
+    //   }
+    // }
     // writeUserData(user.uid);
   }, [user]);
 
